d8528d01cfc556c11c1e08c924fc7183d725dc20
BradOlv <boliva-2023502@kinal.edu.gt>
2026-03-05 22:56:37 -0600

    T20: Implementación de módulo Skill para Client/Worker

---
+++ src/Skill/skill.routes.js
@@ -0,0 +1,11 @@
+
+import { Router } from 'express';
+import { getSkills } from './skill.controller.js';
+import { validateJWT } from '../../middlewares/validate-jwt.js';
+
+const router = Router();
+
+// CLIENT y WORKER: Ver Skills disponibles
+router.get('/', [validateJWT], getSkills);
+
+export default router;
