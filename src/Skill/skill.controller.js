611b0b529138af7a85c82a7fa218a53b0a3a35b4
BradOlv <boliva-2023502@kinal.edu.gt>
2026-03-07 03:59:47 -0600

    T22: crear endpoints en PostMan de la entidad Skill para CLIENT y WORKER

--- src/Skill/skill.controller.js
+++ src/Skill/skill.controller.js
@@ -4,7 +4,9 @@ import Skill from './skill.model.js';
 // CLIENT y WORKER: Ver Skills 
 export const getSkills = async (req, res) => {
     try {
-        const skills = await Skill.find({ isActive: true }).populate('categoryId', 'name');
+        
+        const skills = await Skill.find({ isActive: true }); 
+        
         res.status(200).json({ success: true, data: skills });
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
